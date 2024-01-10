ed4687df77e060bcf2eab6d955edf24d0dc81353
Eisner Puerta~ <eisner.puerta@pragma.com.co>
2024-01-09 22:24:40 -0500

    first commit

---
+++ src/test/java/com/lite/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
